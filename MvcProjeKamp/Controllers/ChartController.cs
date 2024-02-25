63bcb64ee09f9d393e0d9d19be1c8ed3eb993e32
Bilge <bilgegokkaya@icloud.com>
2024-02-25 14:47:03 +0300

    Add project files.

---
+++ MvcProjeKamp/Controllers/ChartController.cs
@@ -0,0 +1,49 @@
+﻿using MvcProjeKamp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKamp.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CategoryChart()
+        {
+            return Json(BlogList(), JsonRequestBehavior.AllowGet);
+        }
+
+        public List<CategoryClass> BlogList()
+        {
+            List<CategoryClass> ct= new List<CategoryClass>();
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "kitap",
+                CategoryCount = 8
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryName="Eğitim",
+                CategoryCount= 4
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "yazılım",
+                CategoryCount = 7
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "spor",
+                CategoryCount = 1
+            });
+            return ct;
+        }
+    }
+}
---
+++ MvcProjeKamp/Controllers/WriterPanelController.cs
@@ -0,0 +1,120 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
+using BusinessLayer.ValidationRules;
+using FluentValidation.Results;
+
+namespace MvcProjeKamp.Controllers
+{
+    public class WriterPanelController : Controller
+    {
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        WriterManager wm =new WriterManager(new EfWriterDal());
+        WriterValidator writervalidator = new WriterValidator();
+        Context c = new Context();
+
+        [HttpGet]
+        public ActionResult WriterProfile(int id=0)
+        {
+            string p = (string)Session["WriterMail"];
+            id = c.Writers.Where(x => x.WriterMail == p).Select(y => y.WriterID).FirstOrDefault();
+            var writevalue=wm.GetByID(id);
+            return View(writevalue);
+        }
+
+        [HttpPost]
+        public ActionResult WriterProfile(Writer p)
+        {
+            ValidationResult results = writervalidator.Validate(p);
+            if (results.IsValid)
+            {
+                wm.WriteUpdate(p);
+                return RedirectToAction("AllHeading","WriterPanel");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+        public ActionResult MyHeading(string p)
+        {
+          
+            p = (string)Session["WriterMail"];
+            var writeridinfo=c.Writers.Where(x=>x.WriterMail==p).Select(y=>y.WriterID).FirstOrDefault();
+            var values = hm.GetListByWriter(writeridinfo);
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult NewHeading()
+        {
+    
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+            ViewBag.vlc = valuecategory;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewHeading(Heading p)
+        {
+            string writermailinfo= (string)Session["WriterMail"];
+            var writeridinfo = c.Writers.Where(x => x.WriterMail == writermailinfo).Select(y => y.WriterID).FirstOrDefault();          
+            p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.WriterID = writeridinfo;
+            p.HeadingStatus = true;
+            hm.HeadingAddBL(p);
+            return RedirectToAction("MyHeading");
+            
+        }
+
+        [HttpGet]
+        public ActionResult EditHeading(int id)
+        {
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+            ViewBag.vlc = valuecategory;
+            var HeadingValue = hm.GetByID(id);
+            return View(HeadingValue);
+        }
+        [HttpPost]
+        public ActionResult EditHeading(Heading p)
+        {
+            hm.HeadingUpdate(p);
+            return RedirectToAction("MyHeading");
+        }
+        public ActionResult DeleteHeading(int id)
+        {
+            var HeadingValue = hm.GetByID(id);
+            HeadingValue.HeadingStatus = false;
+            hm.HeadingDelete(HeadingValue);
+            return RedirectToAction("MyHeading");
+
+        }
+        public ActionResult AllHeading(int p = 1)
+        {
+            var headings = hm.GetList().ToPagedList(p , 4);
+            return View(headings);
+        }
+    }
+}
